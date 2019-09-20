900d764a26657e1b3af9819497298c15c727a446
jansona <alelipona121@gmail.com>
2019-09-20 17:26:26 +0800

    temp commit

---
+++ parser/lexical_analysis/LexParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lexical_analysis
+{
+    class LexParser
+    {
+
+        private List<Token> ParseSentence(string sentence)
+        {
+            List<Token> tokens = new List<Token>();
+
+            foreach(Char c in sentence)
+            {
+                switch (c)
+                {
+                    case ' ':
+                    case '\n':
+                    case '\t':
+                        continue;
+                }
+
+                if(Char.IsLetter(c) || c.Equals('_'))
+                {
+
+                }
+            }
+
+            return tokens;
+        }
+
+        private Token ParseOneWord(string word)
+        {
+            Token token = new Token();
+
+
+
+
+            return token;
+        }
+    }
+}
---
+++ parser/lexical_analysis/Token.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lexical_analysis
+{
+    class Token
+    {
+        private int id;
+        private string name;
+        // 作用域
+        // 数值
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
