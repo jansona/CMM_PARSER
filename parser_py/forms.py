98b82da9c654f54e20030e7b82a16ecba439310a
jansona <alelipona121@gmail.com>
2019-10-11 15:43:35 +0800

    语法分析好起来了

--- parser_py/forms.py
+++ parser_py/forms.py
@@ -11,37 +11,37 @@ def reduce_3():
     return Token(idt="C")
 
 def reduce_4():
-    return Token(idt="C")
+    return Token(idt="L")
 
 def reduce_5():
     return Token(idt="L")
 
 def reduce_6():
-    return Token(idt="C")
+    return Token(idt="L")
 
 def reduce_7():
-    return Token(idt="C")
+    return Token(idt="L")
 
 def reduce_8():
     return Token(idt="L")
 
 def reduce_9():
-    return Token(idt="L")
+    return Token(idt="Z")
 
 def reduce_10():
-    return Token(idt="C")
+    return Token(idt="F")
 
 def reduce_11():
     return Token(idt="F")
 
 def reduce_12():
-    return Token(idt="F")
+    return Token(idt="E")
 
 def reduce_13():
     return Token(idt="E")
 
 def reduce_14():
-    return Token(idt="E")
+    return Token(idt="I")
 
 def reduce_15():
     return Token(idt="I")
@@ -106,4 +106,4 @@ def reduce_34():
 def reduce_35():
     return Token(idt="T")
 
-forms = [(4, reduce_0), (3, reduce_1), (0, reduce_2), (3, reduce_3), (3, reduce_4), (1, reduce_5), (1, reduce_6), (1, reduce_7), (1, reduce_8), (1, reduce_9), (5, reduce_10), (4, reduce_11), (3, reduce_12), (1, reduce_13), (1, reduce_14), (7, reduce_15), (6, reduce_16), (3, reduce_17), (1, reduce_18), (3, reduce_19), (3, reduce_20), (1, reduce_21), (3, reduce_22), (3, reduce_23), (1, reduce_24), (1, reduce_25), (3, reduce_26), (1, reduce_27), (1, reduce_28), (1, reduce_29), (1, reduce_30), (1, reduce_31), (1, reduce_32), (6, reduce_33), (4, reduce_34), (3, reduce_35)]
+forms = [(5, reduce_0), (3, reduce_1), (0, reduce_2), (2, reduce_3), (2, reduce_4), (1, reduce_5), (1, reduce_6), (2, reduce_7), (2, reduce_8), (5, reduce_9), (4, reduce_10), (3, reduce_11), (1, reduce_12), (1, reduce_13), (7, reduce_14), (5, reduce_15), (6, reduce_16), (3, reduce_17), (1, reduce_18), (3, reduce_19), (3, reduce_20), (1, reduce_21), (3, reduce_22), (3, reduce_23), (1, reduce_24), (1, reduce_25), (3, reduce_26), (1, reduce_27), (1, reduce_28), (1, reduce_29), (1, reduce_30), (1, reduce_31), (1, reduce_32), (5, reduce_33), (4, reduce_34), (3, reduce_35)]
